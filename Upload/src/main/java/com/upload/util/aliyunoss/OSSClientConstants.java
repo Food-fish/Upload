08abfb63a070f45e5d6397f0e3f33a3011fbc9c1
Administrator <Administrator@DESKTOP-FA8GBUI>
2017-10-31 13:17:21 +0800

    阿里云Java上传实例

---
+++ Upload/src/main/java/com/upload/util/aliyunoss/OSSClientConstants.java
@@ -0,0 +1,14 @@
+package com.upload.util.aliyunoss;
+
+public class OSSClientConstants {
+	//阿里云API的外网域名  
+    public static final String ENDPOINT = "oss-cn-shenzhen.aliyuncs.com";  
+    //阿里云API的密钥Access Key ID  
+    public static final String ACCESS_KEY_ID = "LTAIcK8YxfNT0YkY";  
+    //阿里云API的密钥Access Key Secret  
+    public static final String ACCESS_KEY_SECRET = "ta705nYHkOVPGlAPTMQTBnNllVOEgw";  
+    //阿里云API的bucket名称  
+    public static final String BACKET_NAME = "jijidom-img";  
+    //阿里云API的文件夹名称  
+    public static final String FOLDER=""; 
+}
